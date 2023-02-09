6b3793ad2aa423b691fbb0d9d48a74fea0b5c44f
VinceStargazer <yu.guoq@northeastern.edu>
2023-02-08 19:19:46 -0800

    added public file for domain verification

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -14,6 +15,7 @@ require("./db");
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan("dev"));
 app.use(cors());
 app.use(errorHandler);
